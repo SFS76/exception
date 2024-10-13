58ce1ee913947133a1a0cd70c0365dd5a4435c58
SFS76 <sfs76@yandex.ru>
2024-10-13 13:29:53 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) {
+
+        String login = "Sfs_";
+        String pass = "123456";
+        String confirmPass = "123456";
+        try {
+            Validate.validate(login, pass, confirmPass);
+        } catch (WrongLoginException wrongLoginException) {
+            System.out.println("Логин не принят");
+        } catch (WrongPasswordException wrongPasswordException) {
+            System.out.println("Пароль не принят");
+        } finally {
+            System.out.println("Валидация завершена");
+        }
 
     }
 }
--- src/Validate.java
+++ src/Validate.java
@@ -1,9 +1,30 @@
-public class Validate {
-    public static void validate (
-            String login;
-            String pass;
-            String confirmPass;
-    ) {
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
 
+import java.util.regex.Pattern;
+
+public class Validate {
+
+    private final static Pattern LOGIN_PATTERN = Pattern.compile("[a-zA-Z0-9_]{1,20}");
+    private final static Pattern PASS_PATTERN = Pattern.compile("[a-zA-Z0-9_]{1,20}");
+
+    public static void validate (String login, String pass, String confirmPass) {
+        validateLogin(login);
+        validatePass(pass, confirmPass);
+    }
+
+
+    private static void validateLogin(String login) {
+        if (!LOGIN_PATTERN.matcher(login).matches()) {
+            throw new WrongLoginException("Неверный формат логина");
+        }
+    }
+    private static void validatePass(String pass, String confirmPass) {
+        if (!PASS_PATTERN.matcher(pass).matches()) {
+            throw new WrongPasswordException("Неверный формат пароля");
+        }
+        if (!pass.equals(confirmPass)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
     }
 }
